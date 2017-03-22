883d0be530ef508ef2f68cbd9013de7abdd2a49d
rkulaszewicz <rkulaszewicz@gmail.com>
2017-03-22 20:19:34 +0100

    Add files via upload

---
+++ Strona/gulpfile.js
@@ -0,0 +1,16 @@
+var gulp = require('gulp');
+var connect = require('connect');
+gulp.task('reload', function(){
+  connect.server({
+    root:'docs',
+    livereload: true,
+  });
+});
+gulp.task('html', function(){
+  gulp.src('./docs/*.html')
+  .pipe(connect.reload());
+});
+gulp.task('watch', function () {
+  gulp.watch(['./docs/*.html'], ['html']);
+});
+gulp.task('default', ['connect', 'watch']);
