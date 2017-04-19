b05c1479e652b50c6da5212d176f6107603f4063
rkulaszewicz <rkulaszewicz@gmail.com>
2017-04-19 18:57:39 +0200

    Add files via upload

---
+++ docs/javascript/tabela.js
@@ -0,0 +1,25 @@
+function loadDoc() {
+var xhttp = new XMLHttpRequest();
+xhttp.onreadystatechange = function() {
+  if (this.readyState == 4 && this.status == 200) {
+    myFunction(this);
+  }
+};
+xhttp.open("GET", "files/tabela.xml", true);
+xhttp.onreadystatechange = handleStateChange;
+xhttp.send();
+}
+function myFunction(xml) {
+var i;
+var xmlDoc = xml.responseXML;
+var table="<tr><th>Artist</th><th>Title</th></tr>";
+var x = xmlDoc.getElementsByTagName("book");
+for (i = 0; i <x.length; i++) {
+  table += "<tr><td>" +
+  x[i].getElementsByTagName("author")[0].childNodes[0].nodeValue +
+  "</td><td>" +
+  x[i].getElementsByTagName("title")[0].childNodes[0].nodeValue +
+  "</td></tr>";
+}
+document.getElementById("demo").innerHTML = table;
+}
