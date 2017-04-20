9ef3fdbda44dcae76a7810adb636afa29d0e43e4
rkulaszewicz <rkulaszewicz@gmail.com>
2017-04-20 20:55:44 +0200

    Update tabela.js

--- docs/javascript/tabela.js
+++ docs/javascript/tabela.js
@@ -28,6 +28,6 @@ for (i = 0; i <x.length; i++) {
   x[i].getElementsByTagName("description")[0].childNodes[0].nodeValue +
   "</td></tr>";
 }
-  table +="</tbody><tfood><tr><td colspan='6'>Dane pobrane ze strony MS. Rafał Kulaszewicz</td></tr></tfood>";
+  table +="<tfood><tr><td colspan='6'>Dane pobrane ze strony MS. Rafał Kulaszewicz</td></tr></tfood></tbody>";
 document.getElementById("demo").innerHTML = table;
 }
