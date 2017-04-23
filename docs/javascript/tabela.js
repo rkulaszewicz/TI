35c24adb4b1641ae83719fc014fcd0b3db4813bb
rkulaszewicz <rkulaszewicz@gmail.com>
2017-04-23 12:47:25 +0200

    Update tabela.js

--- docs/javascript/tabela.js
+++ docs/javascript/tabela.js
@@ -11,7 +11,7 @@ xhttp.send();
 function myFunction(xml) {
 var i;
 var xmlDoc = xml.responseXML;
-var table="<caption>To jest moja tabelka</caption>"
+var table="<caption>To jest moja tabelka</caption>";
 var table="<thead><tr><th>Autor</th><th>Tytuł</th><th>Gatunek</th><th>Cena</th><th id='data'>Data Publikacji</th><th>Opis</th></tr></thead><tbody>";
 var x = xmlDoc.getElementsByTagName("book");
 for (i = 0; i <x.length; i++) {
